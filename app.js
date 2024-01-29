1b3975b276148bfef5ccc77546340d560df018ec
DemetrioPadre <demetriopadre@gmail.com>
2024-01-29 14:14:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            message: 'prima lezione di Vue lunedi 29 gennaio 2024',
+            img: 'gatto.jpeg',
+
+        };
+    }
+}).mount('#app');
