5be61e63995f87b626ce67fb5b892c6beacc958d
DemetrioPadre <demetriopadre@gmail.com>
2024-01-29 15:17:29 +0100

    add-img

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ createApp({
     data() {
         return {
             message: 'prima lezione di Vue lunedi 29 gennaio 2024',
-            img: 'gatto.jpeg',
+            img: './gatto.jpeg',
 
         };
     }
